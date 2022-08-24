683df8dc64c69124ad532a89dcc8a223b63e6332
Parsatag462 <mr.nitsuji462@gmail.com>
2022-08-24 09:31:11 -0400

    gmethod

---
+++ GenericMethodExample/src/GenericMethodClass.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericMethodClass {
+    public <T> void printArray(T[] array){
+        for (T arrayItem : array){
+            System.out.println(arrayItem);
+        }
+    }
+
+    public void printList(List<?> list){
+        for (Object elem: list){
+            System.out.println(elem);
+        }
+        System.out.println();
+    }
+
+    public double sumOfList(ArrayList<? extends Number> arrayList){
+        double s = 0.0;
+        for (Number n : arrayList)
+            s += n.doubleValue();
+
+        return s;
+    }
+
+    public void addNumbers(ArrayList<? super Integer> arrayList){
+        for (int i = 1; i <= 10; i++){
+            arrayList.add(i);
+        }
+    }
+}
