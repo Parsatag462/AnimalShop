33baadcd0194a6f00d9cb631b8d26a592677118c
Parsatag462 <mr.nitsuji462@gmail.com>
2022-08-24 07:14:43 -0400

    assert

---
+++ Assertions/src/AssertEx.java
@@ -0,0 +1,19 @@
+package assertions;
+
+import java.util.Scanner;
+
+public class AssertEx {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        double x = in.nextDouble();
+        in.close();
+
+        assert x < 2_147_483_647.0 : "Number too large to convert to integer";
+
+        System.out.println("Value of x: " + x);
+        int y = (int) (x);
+
+        System.out.println(y);
+    }
+}
