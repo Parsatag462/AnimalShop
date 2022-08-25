e4e86d539cc1a9ac2e519143a18e5f2ad897005e
Parsatag462 <mr.nitsuji462@gmail.com>
2022-08-25 10:40:21 -0400

    LStack

---
+++ LettersStack/src/LettersStack.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class LettersStack {
+    public static void main(String[] args) {
+        LinkedList<String> letterS = new LinkedList<String>();
+
+        letterS.push("A");
+        letterS.push("B");
+        letterS.push("C");
+        letterS.push("D");
+
+        System.out.println("Linked List : "+letterS);
+        System.out.println("Stack Size  : "+letterS.size());
+
+        while (!letterS.isEmpty())
+            System.out.println(letterS.pop());
+        System.out.println("Linked List : "+letterS);
+    }
+}
